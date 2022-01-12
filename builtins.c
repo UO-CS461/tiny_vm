d6edb945426c4bf5477129749a3347f65363b436
Pranav Mathur <pranavmathur99@gmail.com>
2022-01-11 19:49:57 -0800

    Implement integer subtraction and division

--- builtins.c
+++ builtins.c
@@ -632,14 +632,60 @@ obj_ref native_Int_mult(void ) {
     obj_Int other_int = (obj_Int) other;
     log_debug("Multiplying integer values: %d * %d",
            this_int->value, other_int->value);
-    obj_ref sum = new_int(this_int->value * other_int->value);
-    return sum;
+    obj_ref prod = new_int(this_int->value * other_int->value);
+    return prod;
 }
 
 vm_Word method_Int_mult[] = {
         {.instr = vm_op_enter},
         {.instr = vm_op_call_native},
         {.native = native_Int_mult},
+        {.instr = vm_op_return},
+        {.intval = 1}
+};
+
+
+/* Int:mult (new native_method) */
+obj_ref native_Int_sub(void ) {
+    obj_ref this = vm_fp->obj;
+    assert_is_type(this, the_class_Int);
+    obj_Int this_int = (obj_Int) this;
+    obj_ref other = (vm_fp - 1)->obj;
+    assert_is_type(other, the_class_Int);
+    obj_Int other_int = (obj_Int) other;
+    log_debug("Subtracting integer values: %d - %d",
+           this_int->value, other_int->value);
+    obj_ref diff = new_int(this_int->value - other_int->value);
+    return diff;
+}
+
+vm_Word method_Int_sub[] = {
+        {.instr = vm_op_enter},
+        {.instr = vm_op_call_native},
+        {.native = native_Int_sub},
+        {.instr = vm_op_return},
+        {.intval = 1}
+};
+
+
+/* Int:mult (new native_method) */
+obj_ref native_Int_div(void ) {
+    obj_ref this = vm_fp->obj;
+    assert_is_type(this, the_class_Int);
+    obj_Int this_int = (obj_Int) this;
+    obj_ref other = (vm_fp - 1)->obj;
+    assert_is_type(other, the_class_Int);
+    obj_Int other_int = (obj_Int) other;
+    log_debug("Dividing integer values: %d / %d",
+           this_int->value, other_int->value);
+    obj_ref quot = new_int(this_int->value / other_int->value);
+    return quot;
+}
+
+vm_Word method_Int_div[] = {
+        {.instr = vm_op_enter},
+        {.instr = vm_op_call_native},
+        {.native = native_Int_div},
         {.instr = vm_op_return},
         {.intval = 1}
 };
@@ -658,7 +704,9 @@ struct  class_struct  the_class_Int_struct = {
                 method_Int_equals,  // EQUALS
                 method_Int_less, // LESS
                 method_Int_plus,
-                method_Int_mult
+                method_Int_mult,
+                method_Int_sub,
+                method_Int_div
         }
  };
 
